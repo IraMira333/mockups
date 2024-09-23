83d45d63f789cd6a0be53a4564986e83ef4480aa
IraMira333 <irinat333@gmail.com>
2024-09-23 19:54:43 +0300

    Update Header.tsx

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -6,7 +6,7 @@ import { Navbar } from "./NavBar";
 export const Header = () => {
     const locale = useLocale();
     return (
-        <header className="fixed top-0 left-0 z-20 py-[10px]">
+        <header className="fixed top-0 left-0 z-20 py-[10px] tab:py-[30px] pc:py-10 bg-navy">
             <div className="flex justify-between items-center w-[100vw] px-4 tab:px-10 pc:px-[60px]">
                 <Link href={`/${locale}`}>
                     <IconLogo className="cursor-pointer w-[72px] h-5 pc:w-[117px] pc:h-[33px] hover:text-accent" />

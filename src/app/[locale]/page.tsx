c8d41c99a0c95d05213583d9d9a1a4f96a48b2c7
IraMira333 <irinat333@gmail.com>
2024-09-23 19:55:44 +0300

    Update page.tsx

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,9 +1,17 @@
-import { Header } from "@/components/Header/Header";
+import { AboutUs } from "@/components/HomePage/AboutUsSection/AboutUs";
+import { Hero } from "@/components/HomePage/Hero/Hero";
+import { Services } from "@/components/HomePage/ServicesSection/Services";
+import { Toolkits } from "@/components/HomePage/Toolkits/Toolkits";
+import { VideoSection } from "@/components/HomePage/VideoSection/VideoSection";
 
 export default function Home() {
     return (
         <>
-            <Header />
+            <Hero />
+            <AboutUs />
+            <Services />
+            <Toolkits />
+            <VideoSection />
         </>
     );
 }

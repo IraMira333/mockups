55ae7db3402d8bc55c7a64ebc6402c78185c6511
IraMira333 <irinat333@gmail.com>
2024-09-23 19:55:02 +0300

    Update NavMenu.tsx

--- src/components/Header/NavMenu.tsx
+++ src/components/Header/NavMenu.tsx
@@ -11,17 +11,17 @@ export const NavMenu = ({
     const locale = useLocale();
     const t = useTranslations("Menu");
     const menu = [
-        { name: `${t("about")}`, path: `/${locale}/aboutus` },
-        { name: `${t("services")}`, path: `/${locale}/services` },
-        { name: `${t("toolkits")}`, path: `/${locale}/toolkits` },
-        { name: `${t("showreel")}`, path: `/${locale}/showreel` },
+        { name: `${t("about")}`, path: `/${locale}#aboutus` },
+        { name: `${t("services")}`, path: `/${locale}#services` },
+        { name: `${t("toolkits")}`, path: `/${locale}#toolkits` },
+        { name: `${t("showreel")}`, path: `/${locale}#showreel` },
     ];
 
     return (
         <ul className={`${className}`}>
             {menu.map(({ name, path }, index) => {
                 return (
-                    <li key={index}>
+                    <li key={index} className="tab:w-[84px]">
                         <Link
                             href={path}
                             className=" uppercase"

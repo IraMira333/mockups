077ba8d5ba60635ab633111737a98b67330d314d
IraMira333 <irinat333@gmail.com>
2024-09-24 03:23:05 +0300

    Changed default locale

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 import { LocalePrefix } from "next-intl/routing";
-export const defaultLocale = "en" as const;
+export const defaultLocale = "pl" as const;
 export const locales = ["en", "pl"] as const;
 
 export const localePrefix = "as-needed" satisfies LocalePrefix;

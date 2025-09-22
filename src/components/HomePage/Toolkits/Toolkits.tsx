4412892265f7f9ad648f836fabca5149dcadca91
IraMira333 <irinat333@gmail.com>
2025-09-22 16:24:50 +0300

    Update Toolkits.tsx

--- src/components/HomePage/Toolkits/Toolkits.tsx
+++ src/components/HomePage/Toolkits/Toolkits.tsx
@@ -13,22 +13,24 @@ export const Toolkits = () => {
                 <h2 className="tab:w-[50%]">{t("toolkitsTitle")}</h2>
             </div>
             <div className="pc:flex pc:gap-5 pc:justify-center">
-                <WaveAnimation className="border border-white-40  px-2  pc:mb-5 tab:w-[538px] tab:h-[300px] pc:w-[41%]">
-                    <div className="relative z-10">
+                <WaveAnimation className="border border-white-40 tab:gap-10 px-2  pc:mb-5 tab:w-[538px] tab:h-[300px] pc:w-[41%]">
+                    <div className="relative z-10 tab:w-[51%]">
                         <h3 className=" font-genos font-semibold text-3xl uppercase mb-4 tab:text-5xl">
                             {t("beetleTitle")}
                         </h3>
                         <p>{t("beetleText")}</p>
                     </div>
-                    <Image
-                        src="/beetle.png"
-                        alt="beetle"
-                        width={263}
-                        height={235}
-                        className="relative z-10"
-                    />
+                    <div className="tab:w-[48.5%] h-auto relative z-10">
+                        <Image
+                            src="/beetle.png"
+                            alt="beetle"
+                            width={263}
+                            height={235}
+                            className="w-full h-full object-cover object-center"
+                        />
+                    </div>
                 </WaveAnimation>
-                <WaveAnimation className="border border-white-40  px-2  pc:mb-5 tab:w-[688px] tab:h-[327px] pc:h-[300px] pc:w-[57%]">
+                <WaveAnimation className="border border-white-40  px-2 tab:gap-10 pc:mb-5 tab:w-[688px] tab:h-[327px] pc:h-[300px] pc:w-[57%]">
                     <div className="relative z-10">
                         <h3 className="font-genos font-semibold text-3xl uppercase mb-4 tab:text-5xl">
                             {t("flirtTitle")}
@@ -51,34 +53,38 @@ export const Toolkits = () => {
             </div>
             <div className="pc:flex pc:gap-5 pc:justify-center">
                 <WaveAnimation className="border border-white-40 px-0 pc:mb-0 tab:w-[650px] tab:h-[300px] tab:flex-row-reverse tab:gap-10 pc:w-[49%]">
-                    <div className="px-2 relative z-10">
+                    <div className="px-2 relative z-10 tab:w-[51%]">
                         <h3 className="font-genos font-semibold text-3xl uppercase mb-4 tab:text-5xl">
                             {t("yellowScanTitle")}
                         </h3>
                         <p className="mb-4">{t("yellowScanText")}</p>
                     </div>
-                    <Image
-                        src="/yellowscan.png"
-                        alt="Yellow Scan"
-                        width={315}
-                        height={300}
-                        className="tab:w-[48.5%] relative z-10"
-                    />
+                    <div className="tab:w-[48.5%] h-auto relative z-10">
+                        <Image
+                            src="/yellowscan.png"
+                            alt="Yellow Scan"
+                            width={315}
+                            height={300}
+                            className="w-full h-full object-cover object-center"
+                        />
+                    </div>
                 </WaveAnimation>
                 <WaveAnimation className="border border-white-40  px-2  mb-0 tab:w-[647px] tab:h-[300px] tab:flex-row-reverse tab:gap-10 pc:w-[49%]">
-                    <div className="relative z-10">
+                    <div className="relative z-10 tab:w-[51%]">
                         <h3 className=" font-genos font-semibold text-3xl uppercase mb-4 tab:text-5xl">
                             {t("sonyTitle")}
                         </h3>
                         <p>{t("sonyText")}</p>
                     </div>
-                    <Image
-                        src="/sony.png"
-                        alt="Sony"
-                        width={315}
-                        height={300}
-                        className="w-[203px] h-auto mx-auto tab:w-[48.5%] relative z-10"
-                    />
+                    <div className="w-[203px] h-auto mx-auto tab:w-[48.5%] relative z-10">
+                        <Image
+                            src="/sony.png"
+                            alt="Sony"
+                            width={315}
+                            height={300}
+                            className="w-full h-full object-cover object-center"
+                        />
+                    </div>
                 </WaveAnimation>
             </div>
         </Section>
